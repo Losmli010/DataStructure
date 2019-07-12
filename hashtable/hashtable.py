784a39898556bbdd7129b22e16955516d297657d
cdj <1060018592@qq.com>
2019-07-12 18:24:49 +0800

    哈希表

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -1,132 +1,187 @@
 class Entry(object):
-    def __init__(self, key, value):
+    """
+    单向链表
+    """
+    def __init__(self, key, value, next=None):
         self.key = key
         self.value = value
+        self.next = next
+
+    def __str__(self):
+        return "Entry<%r, %r>" % (self.key, self.value)
 
 
 class HashTable(object):
-    def __init__(self, bucket_num):
-        if not isinstance(bucket_num, int) or bucket_num < 0:
-            raise ValueError("Invaild value of bucket num")
+    """
+    基于链表法解决冲突问题的散列表
+    """
+    def __init__(self, capacity=16):
+        """
+        :param capacity: 桶个数
+        """
+        self._capacity = capacity
+        self._buckets = [None] * self._capacity
+        # key-value个数
+        self._size = 0
 
-        self.bucket_num = bucket_num
-        self.buckets = [None for _ in range(self.bucket_num)]
-
-    def __str__(self):
-        string = '{'
-        for bucket in self.buckets:
-            if bucket is not None:
-                for entry in bucket:
-                    if isinstance(entry.key, str):
-                        string += ("'%s': " % entry.key)
-                    else:
-                        string += ("%s: " % entry.key)
-                    if isinstance(entry.value, str):
-                        string += ("'%s', " % entry.value)
-                    else:
-                        string += ("%s, " % entry.value)
-        return string.rstrip(', ') + "}"
-
-    def hash(self, key):
-        if not key:
-            return -1
-        power = 0
-        hashing = 0
-        for char in key:
-            hashing += (ord(char) - 32) * pow(95, power)
-            power += 1
-        index = hashing % self.bucket_num
-        return index
+    def has_key(self, key):
+        """
+        键值是否存在
+        :param key:
+        :return:
+        """
+        idx = self._hash(key)
+        entry = self._buckets[idx]
+        while entry:
+            if entry.key == key:
+                return True
+            entry = entry.next
+        return False
 
     def add(self, key, value):
-        index = self.hash(str(key))
-        if index < 0 or index > self.bucket_num:
-            return False
-        if self.buckets[index] is None:
-            self.buckets[index] = [Entry(key, value)]
-            return True
+        """
+        添加
+        :param key:
+        :param value:
+        :return:
+        """
+        if self._size >= self._capacity:
+            self._resize()
+
+        idx = self._hash(key)
+        # index对应的桶中没有数据
+        if self._buckets[idx] is None:
+            self._buckets[idx] = Entry(key, value)
+            self._size += 1
+            return
         else:
-            for entry in self.buckets[index]:
+            entry = self._buckets[idx]
+            # 第一个就是要找的键
+            if entry.key == key:
+                entry.value = value
+                return
+            while entry.next:
+                # 键值存在，更新值
                 if entry.key == key:
                     entry.value = value
-                    return True
-            self.buckets[index].append(Entry(key, value))
-            return True
+                    return
+                entry = entry.next
+            # 键值不存在，添加到链表尾部
+            entry.next = Entry(key, value)
+            self._size += 1
 
-    def update_value(self, key, value):
-        index = self.hash(str(key))
-        if index is None or self.buckets[index] is None:
-            # print("Key Not Found!")
-            return False
-        else:
-            for entry in self.buckets[index]:
+    def remove(self, key):
+        """
+        删除
+        :param key:
+        :return:
+        """
+        if self.has_key(key):
+            idx = self._hash(key)
+            entry = self._buckets[idx]
+            # 第一个节点就是要删除的节点
+            if entry.key == key:
+                self._buckets[idx] = entry.next
+                self._size -= 1
+                return entry.value
+            # 删除节点为链表中间节点
+            prev = entry
+            entry = entry.next
+            while entry:
                 if entry.key == key:
-                    entry.value = value
-                    return True
-            # print("Key Not Found!")
-            return False
-
-    def delete(self, key):
-        index = self.hash(str(key))
-        if index is None or self.buckets[index] is None:
-            # print("Key Not Found!")
-            return False
-        else:
-            for entry in self.buckets[index]:
-                if entry.key == key:
-                    self.buckets[index].remove(entry)
-                    return True
-            # print("Key Not Found!")
-            return False
-
-    def look_up(self, key):
-        index = self.hash(str(key))
-        if index is None or self.buckets[index] is None:
-            # print("Key Not Found!")
-            return False
-        else:
-            for entry in self.buckets[index]:
-                if entry.key == key:
+                    prev.next = entry.next
+                    self._size -= 1
                     return entry.value
-            # print("Key Not Found!")
-            return False
+                prve = entry
+                entry = entry.next
 
-    def print_distribution(self):
-        string = ''
-        bucket_num = 0
-        mins = None
-        maxs = 0
-        total = 0
-        for bucket in self.buckets:
-            if bucket is not None:
-                # string += ("Bucket Number %d has: "%bucketNum)
-                counts = 0
-                for entry in bucket:
-                    counts += 1
-                # string += ("%d entries\n"%count)
-                total += counts
-                if counts > maxs:
-                    maxs = counts
-                if mins is None or counts < mins:
-                    mins = counts
-            else:
-                pass
-                # string += ("Bucket Number %d has 0 entries\n"%(bucketNum))
-            bucket_num += 1
-        string += ("Largest Bucket has %d entries\n" \
-                   "Smallest Bucket has %d entries\nTotal entries: %d\n" \
-                   "Avg bucket size is %f" % (maxs, mins, total, (total / self.bucket_num)))
-        return string
+    def get(self, key):
+        """
+        查找
+        :param key:
+        :return:
+        """
+        idx = self._hash(key)
+        entry = self._buckets[idx]
+        while entry:
+            if entry.key == key:
+                return entry.value
+            entry = entry.next
+
+    def _resize(self):
+        """
+        进行扩容: 当键值对数量超过容量大小时进行扩容
+        :return:
+        """
+        # 扩容为原来的2倍
+        old_capacity = self._capacity
+        self._capacity = 2 * old_capacity
+        new_buckets = [None] * self._capacity
+        old_buckets = self._buckets
+        self._transfer(old_buckets, new_buckets)
+        self._buckets = new_buckets
+
+    def _transfer(self, old_buckets, new_buckets):
+        # 取桶中的链表
+        for entry in old_buckets:
+            # 取链表中的键值对
+            while entry:
+                idx = self._hash(entry.key)
+                new_entry = new_buckets[idx]
+                if new_entry is None:
+                    new_buckets[idx] = Entry(entry.key, entry.value)
+                else:
+                    while new_entry.next:
+                        new_entry = new_entry.next
+                    new_entry.next = Entry(entry.key, entry.value)
+                entry = entry.next
+
+    def _hash(self, key):
+        """
+        哈希函数
+        :param key:
+        :return:
+        """
+        power = 0
+        hashing = 0
+        for char in str(key):
+            hashing += (ord(char) - 32) * pow(95, power)
+            power += 1
+        index = hashing % self._capacity
+        return index
+
+    def __iter__(self):
+        result = []
+        for entry in self._buckets:
+            if entry is not None:
+                while entry:
+                    yield entry
+                    entry = entry.next
+        return result
+
+    def __str__(self):
+        result = []
+        for entry in self._buckets:
+            if entry is not None:
+                while entry:
+                    result.append((entry.key, entry.value))
+                    entry = entry.next
+        return str(result)
 
 
 if __name__ == '__main__':
-    ht = HashTable(100)
-    print(ht)
+    dictionary = HashTable()
+    print("*************添加键值对********************")
+    for key, value in enumerate(range(100, 140)):
+        dictionary.add(key, value)
+    print(dictionary)
 
-    ht.add('bob', 2)
-    ht.add(11, 3)
-    ht.add('11', 4)
-    ht.add(' ', 5)
-    print(ht)
-    print(ht.look_up(11))
-    print(ht.print_distribution())
+    print("*************获取键值********************")
+    print(dictionary.get(10))
+
+    print("*************修改键值********************")
+    print(dictionary.add(10, 1000), dictionary.get(10))
+
+    print("*************删除键值********************")
+    for key in range(40):
+        print(dictionary.remove(key), dictionary)
