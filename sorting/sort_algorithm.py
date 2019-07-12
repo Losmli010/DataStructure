2a0b266c89158d9a3b47b780107060da54d0fb32
cdj <1060018592@qq.com>
2019-07-12 10:57:15 +0800

    排序算法

--- sorting/sort_algorithm.py
+++ sorting/sort_algorithm.py
@@ -1,3 +1,4 @@
+# http://www.cnblogs.com/eniac12/p/5329396.html
 """
 冒泡排序: 从左向右每次循环找出当前数列中的最大值
 时间复杂度: O(n^2)
@@ -8,20 +9,22 @@
 4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
 """
 
-#交换数组中两个数的位置
+
 def swap(arr, i, j):
     temp = arr[i]
     arr[i] = arr[j]
     arr[j] = temp
     return arr
 
+
 def bubble_sort(arr):
     length = len(arr)
-    for i in range(length - 1):               #每次最大元素就像气泡一样"浮"到数组的最后
-        for j in range(length - 1 - i):       #依次比较相邻的两个元素,使较大的那个向后移
+    for i in range(length - 1):               # 每次最大元素就像气泡一样"浮"到数组的最后
+        for j in range(length - 1 - i):       # 依次比较相邻的两个元素,使较大的那个向后移
             if arr[j] > arr[j + 1]:
                 swap(arr, j, j + 1)
     return arr
+
 
 """
 鸡尾酒排序:从左向右循环找出最大值,从右向左循环找出最小值
@@ -34,19 +37,21 @@ def bubble_sort(arr):
 4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
 """
 
+
 def cocktail_sort(arr):
     left = 0
     right = len(arr) - 1
     while left < right:
-        for i in range(left, right):                #从左向右,较大数值向后移动
+        for i in range(left, right):                # 从左向右,较大数值向后移动
             if arr[i] > arr[i + 1]:
                 swap(arr, i, i + 1)
         right -= 1
-        for j in range(right, left, -1):            #从右向左,较小数值先前移动
+        for j in range(right, left, -1):            # 从右向左,较小数值先前移动
             if arr[j - 1] > arr[j]:
                 swap(arr, j - 1, j)
         left += 1
     return arr
+
 
 """
 选择排序: 从左向右循环找出最小值保存到固定位置
@@ -56,13 +61,15 @@ def cocktail_sort(arr):
 2.针对所有的元素重复以上的步骤，除了最后一个。
 """
 
+
 def select_sort(arr):
     length = len(arr)
     for i in range(length - 1):
-        for j in range(i + 1, length):         #将左边的第一个数和后面的数分别比较,将最小的数交换到最左边
+        for j in range(i + 1, length):         # 将左边的第一个数和后面的数分别比较,将最小的数交换到最左边
             if arr[i] > arr[j]:
                 swap(arr, i, j)
     return arr
+
 
 """
 插入排序: 左手已经拿到一张牌,将右手每次拿到的牌按升序插入正确的位置
@@ -74,25 +81,28 @@ def select_sort(arr):
 4.重复步骤1~3
 """
 
+
 def insert_sort(arr):
     length = len(arr)
-    for i in range(1, length):                 #右手拿到一张牌拿在手里
+    for i in range(1, length):                 # 右手拿到一张牌拿在手里
         j = i - 1
         temp = arr[i]
-        while arr[j] > temp and j >= 0:        #从右现在,将右手的牌和前面的牌依次比较
-            arr[j + 1] = arr[j]                #右手的牌较小,向后移动前面的牌
+        while arr[j] > temp and j >= 0:        # 从右现在,将右手的牌和前面的牌依次比较
+            arr[j + 1] = arr[j]                # 右手的牌较小,向后移动前面的牌
             j -= 1
-        arr[j + 1] = temp                      #将右手的牌插入此位置
+        arr[j + 1] = temp                      # 将右手的牌插入此位置
     return arr
+
 
 """
 希尔排序: 一次将较小的数据向前移动h位
 时间复杂度: O(n^2)
 """
 
+
 def shell_sort(arr):
     length = len(arr)
-    h = length // 2                                   #初始步长
+    h = length // 2                                   # 初始步长
     while h >= 1:
         for i in range(h, length):
             j = i - h
@@ -101,13 +111,15 @@ def shell_sort(arr):
                 arr[j + h] = arr[j]
                 j -= h
             arr[j + h] = temp
-        h = h // 2                                    #递减步长
+        h = h // 2                                    # 递减步长
     return arr
+
 
 """
 归并排序
 时间复杂度: O(nlogn)
 """
+
 
 def merge(left, right):
     i, j = 0, 0
@@ -123,6 +135,7 @@ def merge(left, right):
     temp += right[j:]
     return temp
 
+
 def merge_sort(arr):
     if len(arr) == 1:
         return arr
@@ -132,6 +145,7 @@ def merge_sort(arr):
     right = merge_sort(arr[middle:])
     return merge(left, right)
 
+
 """
 快速排序
 时间复杂度: O(nlogn)
@@ -140,6 +154,7 @@ def merge_sort(arr):
 2.把所有比基准值小的元素放在基准前面，所有比基准值大的元素放在基准的后面（相同的数可以到任一边）
 3.对每个分区递归地进行步骤1~2，递归的结束条件是序列的大小是0或1
 """
+
 
 def quick_sort(arr, left, right):
     if left >= right:
@@ -162,10 +177,12 @@ def quick_sort(arr, left, right):
     quick_sort(arr, low + 1, right)
     return arr
 
+
 """
 二分法查找
 假设: 数据已经经过排序,且为升序
 """
+
 
 def binary_search(arr, target):
     left = 0
@@ -179,5 +196,3 @@ def binary_search(arr, target):
         else:
             return middle
     return -1
-
-#http://www.cnblogs.com/eniac12/p/5329396.html
