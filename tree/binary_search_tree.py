696162ebdc5350ed8014569b0e19780a3818e8e8
cdj <1060018592@qq.com>
2019-07-19 23:25:24 +0800

    二叉查找树

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -71,19 +71,20 @@ class BinarySearchTree(object):
                 if (cur.left is None) and (cur.right is None):
                     if cur.parent is None:      # 删除节点为根节点
                         self.root = None
-                        return
-                    elif cur.parent.left.key == key:    # 删除节点为左叶子节点
+                    elif cur.parent.key > key:    # 删除节点为左叶子节点
                         cur.parent.left = None
                     else:
                         cur.parent.right = None
+                    return
                 # cur有左子树无右子树
                 elif (cur.left is not None) and (cur.right is None):
                     if cur.parent is None:      # 删除节点为根节点
                         self.root = self.root.left
-                    elif cur.parent.left.key == key:  # 删除节点为左子树节点
+                    elif cur.parent.key > key:  # 删除节点为左子树节点
                         cur.parent.left = cur.left
                     else:
                         cur.parent.right = cur.left
+                    return
                 # cur有右子树无左子树
                 elif (cur.left is None) and (cur.right is not None):
                     if cur.parent is None:  # 删除节点为根节点
@@ -92,16 +93,16 @@ class BinarySearchTree(object):
                         cur.parent.left = cur.right
                     else:
                         cur.parent.right = cur.right
+                    return
                 # cur有左右子树
                 elif (cur.left is not None) and (cur.right is not None):
                     # 找到右子树中的最小节点并交换key
-                    val = copy(cur.key)
-                    temp = copy(cur.right)
+                    temp = cur.right
                     while temp.left:
                         temp = temp.left
                     cur.key = temp.key
+                    temp.key = key
                     cur = temp
-                    cur.key = val
 
     def get(self, key):
         """
@@ -140,50 +141,92 @@ class BinarySearchTree(object):
             cur = cur.right
         return cur
 
-    def preorder(self, node):
+    def preorder(self):
         """
         前序遍历：根节点->左子树->右子树
         :return:
         """
-        if node is not None:
-            yield node.key
-            yield self.preorder(node.left)
-            yield self.preorder(node.right)
+        if self.root is None:
+            return []
 
-    def inorder(self, node):
+        result = []
+        stack = [self.root]
+
+        cur = self.root
+        while stack:
+            result.append(cur.key)
+            if cur.right is not None:
+                stack.append(cur.right)
+            if cur.left is not None:
+                stack.append(cur.left)
+
+            cur = stack.pop()
+        return result
+
+    def inorder(self):
         """
         中序遍历：左子树->根节点->右子树
         :return:
         """
-        if node is not None:
-            yield self.inorder(node.left)
-            yield node.key
-            yield self.inorder(node.right)
+        if self.root is None:
+            return []
 
-    def postorder(self, node):
+        result = []
+        stack = []
+
+        cur = self.root
+        while cur or stack:
+            if cur is None:
+                cur = stack.pop()
+                result.append(cur.key)
+                cur = cur.right
+            else:
+                stack.append(cur)
+                cur = cur.left
+        return result
+
+    def postorder(self):
         """
         后序遍历：左子树->右子树->根节点
         :return:
         """
-        if node is not None:
-            yield self.postorder(node.left)
-            yield self.postorder(node.right)
-            yield node.key
+        if self.root is None:
+            return []
+
+        stack = [self.root]
+        stack2 = []
+        result = []
+        while stack:
+            cur = stack.pop()
+            stack2.append(cur)
+            if cur.left:
+                stack.append(cur.left)
+            if cur.right:
+                stack.append(cur.right)
+
+        while stack2:
+            node = stack2.pop()
+            result.append(node.key)
+        return result
 
     def __str__(self):
-        result = ""
-        cur = self.root
-        while cur:
-            result += "-->%s" % cur.key
-            left = cur.left
-            while left:
-                left = left.left
-
-        return result
+        preorder = self.preorder()
+        inorder = self.inorder()
+        postorder = self.postorder()
+        return "Pre-order: %r\n" \
+               "In-order: %r\n" \
+               "Post-order: %r" % (preorder, inorder, postorder)
 
 if __name__ == '__main__':
     tree = BinarySearchTree()
-    for i in range(10):
+    print("****************插入*********************")
+    for i in [10, 22, 4, 6, 36, 75, 79, 64, 14, 33]:
         tree.insert(i)
-        print(tree.get(i))
-        print(tree.remove(i))
+    print(tree)
+
+    print("****************获取/删除*********************")
+    for i in [6, 33, 79, 22, 10]:
+        t = tree.get(i)
+        print(t)
+        tree.remove(i)
+        print(tree)
